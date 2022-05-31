54f9b86a48095fd3bb11fbfc30a1274c3dba20a3
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-05-31 08:50:24 +0200

    get_data completed

---
+++ src/stage_01_get_data.py
@@ -0,0 +1,57 @@
+import argparse  
+import shutil 
+import logging 
+from tqdm import tqdm  
+import os 
+import random 
+from src.utils.common import read_yaml, create_directories, unzip_file
+from src.utils.data_mgmt import validating_image
+import urllib.request as req 
+
+STAGE="GET_DATA"  
+
+logging.basicConfig(
+    filename=os.path.join("logs","running_logs.log"),
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+)
+def main(config_path):
+    config=read_yaml(config_path)
+    URL=config["data"]["source_url"]
+    local_dir=config["data"]["local_dir"]
+    create_directories([local_dir])
+    data_file=config["data"]["data_file"]
+    data_file_path=os.path.join(local_dir,data_file)
+    
+    if not os.path.isfile(data_file_path):
+        logging.info(f"Download started .............................")
+        filename, headers=req.urlretrieve(URL,data_file_path)
+        logging.info(f"filename: {filename} created with info \n{headers}")
+    else:
+        logging.info(f"filename: {data_file} already present.")
+    
+###unziping data################################
+    unzip_data_dir=config["data"]["unzip_data_dir"]
+    print(unzip_data_dir)
+    create_directories([unzip_data_dir])
+    unzip_file(source=data_file_path,dest=unzip_data_dir)
+    #### validating data#######  
+    validating_image(config)
+
+
+
+if __name__=='__main__':
+    args=argparse.ArgumentParser()
+    args.add_argument("--config","-c", default="configs/config.yaml")
+    parsed_args=args.parse_args()
+
+
+    try:
+        logging.info("\n*********************************")
+        logging.info(f">>>> stage {STAGE} started <<<<")
+        main(config_path=parsed_args.config)
+        logging.info(f">>>> stage {STAGE} completed <<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
